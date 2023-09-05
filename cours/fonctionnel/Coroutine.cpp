c869412b65b2a5704c17b057f2842c61ab8c3942
Dominique Marcadet <dominique.marcadet@centralesupelec.fr>
2023-09-05 11:23:59 +0200

    add coroutine examples

---
+++ cours/fonctionnel/Coroutine.cpp
@@ -0,0 +1,41 @@
+/*
+ * Cursus CentraleSupélec - Dominante Informatique et numérique
+ * 3IF1020 - Concepts des langages de programmation - Chapitre n°3
+ * 
+ * Coroutine.cpp
+ *
+ * Using library https://github.com/andreasbuhr/cppcoro
+ *
+ * c++ -std=c++20 Coroutine_1.cpp -o Coroutine_1
+ * ./Coroutine_1
+ */
+
+#include <iostream>
+#include <coroutine>
+
+#include <cppcoro/task.hpp>
+#include <cppcoro/sync_wait.hpp>
+
+cppcoro::task<> counter()
+{
+    for( unsigned i{ 1 }; ; ++i ) {
+        std::cout << "counter: resumed (#" << i << ")\n";
+        co_await std::suspend_always{};
+    }
+}
+
+cppcoro::task<> co_main()
+{
+    auto my_counter{ counter() };
+    std::cout << "co_main: resuming counter\n";
+    my_counter.when_ready().m_coroutine.resume();
+    std::cout << "co_main: resuming counter\n";
+    my_counter.when_ready().m_coroutine.resume();
+    co_return;
+}
+
+int main()
+{
+    cppcoro::sync_wait( co_main() );
+}
+
---
+++ cours/fonctionnel/Fibonacci.cpp
@@ -0,0 +1,37 @@
+/*
+ * Cursus CentraleSupélec - Dominante Informatique et numérique
+ * 3IF1020 - Concepts des langages de programmation - Chapitre n°3
+ * 
+ * Example from https://github.com/andreasbuhr/cppcoro
+ *
+ * Fibonacci.cpp
+ *
+ * c++ -std=c++20 Fibonacci.cpp -o Fibonacci
+ * ./Fibonacci
+ */
+
+
+#include <iostream>
+
+#include <cppcoro/generator.hpp>
+
+
+cppcoro::generator< std::uint64_t > fibonacci()
+{
+  std::uint64_t a = 0, b = 1;
+  while( true ) {
+    co_yield b;
+    auto tmp = a;
+    a = b;
+    b += tmp;
+  }
+}
+
+int main()
+{
+  for( auto i : fibonacci() ) {
+    if( i > 1'000'000 ) break;
+    std::cout << i << std::endl;
+  }
+}
+
