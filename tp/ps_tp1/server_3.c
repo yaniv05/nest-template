33bad7715d76d6823e4765bf558b5d8d2073f744
Benchetrit Yaniv <yaniv.benchetrit@student-cs.fr>
2023-10-22 21:19:17 +0200

    Q2.1

--- tp/ps_tp1/server_3.c
+++ tp/ps_tp1/server_3.c
@@ -12,17 +12,27 @@
 #include <signal.h>
 #include <stdbool.h>
 
-// Variable globale qui détermine si la boucle doit continuer à s'exécuter
+// Variables globales
 volatile bool running = true;
+pid_t child_pid;
 
 // Gestionnaire pour le signal SIGINT
 void stop_handler(int sig) {
-    if (getpid() == getpgid(0)) {
-        printf("[Père] Signal %d reçu. Arrêt en cours\n", sig);
-    } else {
-        printf("[Fils] Signal %d reçu. Arrêt en cours\n", sig);
+    if (getpid() != child_pid) { // Père
+        printf("[Père] Signal %d reçu. Arrêt du père...\n", sig);
+        if (running) {
+            kill(child_pid, SIGINT);  // Tuer le fils uniquement si running est vrai
+            printf("Arrêt du fils suite à l'arrêt du père ...\n");
+        }
+    } else {  // Fils
+        printf("[Fils] Signal %d reçu. Arrêt en cours...\n", sig);
     }
-    running = false;
+    running = 0;
+}
+
+void child_exit_handler(int sig) {
+    printf("[Père] Le fils a terminé. Arrêt en cours...\n");
+    running = 0;
 }
 
 void exit_message(void) {
@@ -38,6 +48,13 @@ int main()
     sa.sa_handler = stop_handler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
+
+    //Pour gerer le signal arret du fils
+    struct sigaction sa_child;
+    sa_child.sa_handler = child_exit_handler;
+    sigemptyset(&sa_child.sa_mask);
+    sa_child.sa_flags = 0;
+    sigaction(SIGCHLD, &sa_child, NULL);
 
     //Gestion de SIGINT
     if (sigaction(SIGINT, &sa, NULL) == -1) {
@@ -59,14 +76,14 @@ int main()
     //Affichage du premier message
     printf("Début du TP1 : Hello World \n");
 
-    pid_t pid = fork();
+    child_pid = fork();
 
-    if (pid == 0) {  // Fils
+    if (child_pid == 0) {  // Fils
         while (running) {
             printf("[Fils] PID: %d, PPID: %d, PGID: %d\n", getpid(), getppid(), getpgid(0));
             sleep(1);
         }
-    } else if (pid > 0) {  // Père
+    } else if (child_pid > 0) {  // Père
         while (running) {
             printf("[Père] PID: %d, PPID: %d, PGID: %d\n", getpid(), getppid(), getpgid(0));
             sleep(1);
@@ -76,7 +93,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    printf("Fin du programme, %s se termine.\n", (pid == 0) ? "Fils" : "Père");
+    printf("Fin du programme, %s se termine.\n", (child_pid == 0) ? "Fils" : "Père");
         
     return EXIT_SUCCESS;
 }
@@ -85,4 +102,11 @@ Réponses aux questions
 
 1)CTRL C envoie un SIGINT à tous les processus du groupe dans le terminal en cours, le fils héritant du groupe du processus du père, les deux s'arrêtent donc.
 
+2)On remarque que le processus fils s'arrête, cependant il est toujours listé de manière diférente. Il s'agit d'un processus zombie:
+avant le kill : yanivbenchetrit 90827 90826 90826      0    1 S+   s004    0:00.00 ./server_3
+apres le kill : yanivbenchetrit 90827 90826 90826      0    1 Z+   s004    0:00.00 (server_3)
+
+3) Cette fois ci le fils n'est plus visible
+
+4)On observe que lorsque le père s'arrête, le fils lui continue de tourner.
 */
