ff9f1d9f102f2a08a9d49f6cd5500ea4fdae1c36
Benchetrit Yaniv <yaniv.benchetrit@student-cs.fr>
2023-11-05 11:18:05 +0100

    Q2

--- tp/clp_tp2/Number.hpp
+++ tp/clp_tp2/Number.hpp
@@ -12,21 +12,68 @@
 #include <string>
 #include <utility>
 
+
 class Number {
 public:
-    Number( unsigned long l ) { /* TODO */ }
-    ~Number() { /* TODO */ }
- 
-    void print( std::ostream & out ) const { /* TODO */ }
+    Number( unsigned long l )
+        : first_(nullptr)
+    {
+        if (l == 0) {
+            first_ = new Digit(0);
+        } else {
+            while (l > 0) {
+                addDigitToFront(l % number_base);
+                l /= number_base;
+            }
+        }
+    }
+
+    ~Number() {
+        deleteDigits(first_);
+    }
+
+    void print( std::ostream & out ) const {
+        if (first_) {
+            first_->print(out);
+        }
+    }
 
 private:
     using DigitType = unsigned int;
-    // Un seul chiffre décimal par maillon : l'objectif ici n'est pas la performance
     static const DigitType number_base{ 10u };
+
     struct Digit {
+        Digit(DigitType d)
+            : digit_(d), next_(nullptr) {}
+
+        ~Digit() {
+            // Le destructeur de Digit n'a rien à faire ici car nous gérons la libération de la mémoire dans la fonction deleteDigits
+        }
+
         DigitType digit_;
         Digit * next_;
+
+        void print(std::ostream & out) const {
+            out << digit_; // Afficher d'abord le chiffre courant
+            if (next_) {
+                next_->print(out);
+            }
+        }
     };
+
+    void addDigitToFront(DigitType d) {
+        Digit* newDigit = new Digit(d);
+        newDigit->next_ = first_;
+        first_ = newDigit;
+    }
+
+    void deleteDigits(Digit* current) {
+        if (current) {
+            deleteDigits(current->next_);
+            delete current;
+        }
+    }
+
     Digit * first_;
 };
 
