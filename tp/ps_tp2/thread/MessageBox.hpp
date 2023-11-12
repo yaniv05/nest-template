d8e562c797c598ba31fd145dc9a8e8f88eba128d
Benchetrit Yaniv <yaniv.benchetrit@student-cs.fr>
2023-11-12 12:39:12 +0100

    fin Q3

--- tp/ps_tp2/thread/MessageBox.hpp
+++ tp/ps_tp2/thread/MessageBox.hpp
@@ -17,23 +17,49 @@
  * Version pour synchronisation entre threads
  */
 class MessageBox : public BasicMessageBox {
-public:
-    void put( int message ) {
+public:  
+    void put(int message) {
         // TODO :
         // - Ajouter les instructions de synchronisation
         // - Ne pas faire d'affichage dans cette méthode
-        basic_put( message );
+        std::unique_lock<std::mutex> lock(mtx);
+        while (isFull()) {
+            notFull.wait(lock);
+        }
+        basic_put(message);
+        ++count;  // Augmenter le nombre de messages après avoir déposé un message
+        notEmpty.notify_one();
     }
- 
+
     int get() {
         // TODO :
         // - Ajouter les instructions de synchronisation
         // - Ne pas faire d'affichage dans cette méthode
-        int message{ basic_get() };
+        std::unique_lock<std::mutex> lock(mtx);
+        while (isEmpty()) {
+            notEmpty.wait(lock);
+        }
+        int message = basic_get();
+        --count;  // Diminuer le nombre de messages après avoir retiré un message
+        notFull.notify_one();
         return message;
     }
+
 private:
     // TODO : 
     // - Ajouter les objets de synchronisation
+    std::mutex mtx;  // Mutex pour la synchronisation
+    std::condition_variable notFull;  // Condition pour la disponibilité de l'espace
+    std::condition_variable notEmpty;  // Condition pour la disponibilité du message
+
+    unsigned int count = 0;  // Variable pour suivre le nombre de messages dans la boîte
+
+    bool isFull() const {
+        return count == box_size_;  // La boîte est pleine si le compte atteint la taille de la boîte
+    }
+
+    bool isEmpty() const {
+        return count == 0;  // La boîte est vide si le compte est 0
+    }
 };
  
