5daacaaea1b380c3875e89f12d5791d76834de4e
yaniv05 <yanivbenchetrit@macbook-pro-6.home>
2023-11-27 11:49:02 +0100

    tp 3 Q1

--- tp/clp_tp1/listes.cpp
+++ tp/clp_tp1/listes.cpp
@@ -12,13 +12,46 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <vector>
 
+#include <iostream>
+#include <vector>
 
+#include <iostream>
+#include <vector>
+
+#include <iostream>
+#include <vector>
+
+// Nommons notre type pour plus de facilité d'écriture
+using int_tab_t = std::vector<int>;
+
+// Fonction pour afficher un tableau
+void print_tab(const int_tab_t& tab) {
+    std::cout << "[ ";
+    for (int num : tab) {
+        std::cout << num << " ";
+    }
+    std::cout << "]";
+}
+
+// Fonction de test demandée
+void test_11() {
+    std::cout << "*** test_11 ***" << std::endl;
+    
+    // Création du tableau
+    const int_tab_t tab{ 1, -2, 3, -4, 5, -6 };
+    
+    // Affichage du tableau
+    print_tab(tab);
+    std::cout << std::endl; // Passage à la ligne pour avoir une sortie propre
+}
 
 int main()
 {
     std::srand( std::time( nullptr ));
 
+    test_11();
     return 0;
 }
 
--- tp/clp_tp3/Expression.hpp
+++ tp/clp_tp3/Expression.hpp
@@ -15,7 +15,47 @@
 class Expression {
 public:
     // TODO
+    virtual ~Expression() {}
+    virtual Expression* clone() const = 0;
 private:
 };
+
+class Nombre : public Expression {
+    int valeur;
+
+public:
+    Nombre(int val) : valeur(val) {}
+    Nombre(const Nombre& other) : valeur(other.valeur) {}
+    ~Nombre() override {}
+
+    Nombre* clone() const override {
+        return new Nombre(*this);
+    }
+
+    int getValeur() const {
+        return valeur;
+    }
+};
+
+class Variable : public Expression {
+    std::string nom;
+
+public:
+    Variable(const std::string& nom) : nom(nom) {}
+    Variable(const Variable& other) : nom(other.nom) {}
+    ~Variable() override {}
+
+    Variable* clone() const override {
+        return new Variable(*this);
+    }
+
+    std::string getNom() const {
+        return nom;
+    }
+
+};
+
+
+
 
 #endif
